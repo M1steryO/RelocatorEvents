943c8cb32eec2f7bb5d3e4e2898e70d208bc3ec4
dmitrypeschansky <pda1205@gmail.com>
2026-03-16 23:19:07 +0300

    some changes

--- events/internal/interceptor/auth.go
+++ events/internal/interceptor/auth.go
@@ -2,6 +2,8 @@ package interceptor
 
 import (
 	"context"
+	"fmt"
+	"github.com/M1steryO/RelocatorEvents/events/internal/core/logger"
 	"github.com/M1steryO/platform_common/pkg/sys"
 	"github.com/M1steryO/platform_common/pkg/sys/codes"
 	"google.golang.org/grpc"
@@ -16,15 +18,19 @@ func AuthInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServe
 	}
 
 	userIdMetadata, ok := md["x-user-id"]
+	logger.Info(fmt.Sprint(userIdMetadata))
 	if !ok {
+		logger.Info("x-user-id is not provided")
 		return nil, sys.NewCommonError("metadata is not provided", codes.Unauthenticated)
 	}
 	if len(userIdMetadata) != 1 {
+		logger.Info("x-user-id is not provided 2")
 		return nil, sys.NewCommonError("metadata is not provided", codes.Unauthenticated)
 	}
 
 	userId, err := strconv.ParseInt(userIdMetadata[0], 10, 64)
 	if err != nil {
+		logger.Info("x-user-id is not provided 3")
 		return nil, sys.NewCommonError("metadata is not provided", codes.Unauthenticated)
 	}
 
