c3b037de713d348b5678ddb57ebe1b11906bc9a9
dmitrypeschansky <pda1205@gmail.com>
2026-05-01 21:43:55 +0300

    some changes

--- users/internal/service/user/update_password.go
+++ users/internal/service/user/update_password.go
@@ -2,30 +2,31 @@ package user
 
 import (
 	"context"
+	"errors"
 	domain "github.com/M1steryO/RelocatorEvents/users/internal/models/user"
 	"github.com/M1steryO/RelocatorEvents/users/internal/service/user/dto"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func (s *serv) UpdatePassword(ctx context.Context, userId int64, passwords *dto.UpdatePassword) error {
-	hashedOldPassword, err := bcrypt.GenerateFromPassword([]byte(passwords.OldPassword), bcrypt.DefaultCost)
-	if err != nil {
-		return err
-	}
 
 	hashedNewPassword, err := bcrypt.GenerateFromPassword([]byte(passwords.NewPassword), bcrypt.DefaultCost)
 	if err != nil {
 		return err
 	}
-
+	
 	err = s.txManager.ReadCommitted(ctx, func(ctx context.Context) error {
 		user, err := s.db.Get(ctx, userId)
 		if err != nil {
 			return err
 		}
 
-		if user.Password != string(hashedOldPassword) {
-			return domain.ErrIncorrectOldPassword
+		if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(passwords.OldPassword)); err != nil {
+			if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+
+				return domain.ErrIncorrectOldPassword
+			}
+			return err
 		}
 
 		err = s.db.UpdatePassword(ctx, userId, string(hashedNewPassword))
